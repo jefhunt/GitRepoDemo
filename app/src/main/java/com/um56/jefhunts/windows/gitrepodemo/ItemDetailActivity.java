096c52737b4b67adab4606a95ebac2f7225d982f
jefhunt <umesh1401056@gmail.com>
2017-09-25 23:11:08 +0530

    small change

--- app/src/main/java/com/um56/jefhunts/windows/gitrepodemo/ItemDetailActivity.java
+++ app/src/main/java/com/um56/jefhunts/windows/gitrepodemo/ItemDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
@@ -17,6 +18,8 @@ import android.view.MenuItem;
  * in a {@link ItemListActivity}.
  */
 public class ItemDetailActivity extends AppCompatActivity {
+
+    private static final String TAG = "You too";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,9 @@ public class ItemDetailActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        Log.i(TAG,"Hello git");
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
